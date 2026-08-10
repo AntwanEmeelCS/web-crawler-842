770716a95d19487a74d619173bea974f849c23c1
Antonio2024 <Antonio2024@DESKTOP-E4TO8N0>
2026-08-10 23:35:57 +0300

    Assignment10_FinishedAbout_BeforeAnim

--- EngAhmedWessam/Assignment10/js/scripts.js
+++ EngAhmedWessam/Assignment10/js/scripts.js
@@ -3,22 +3,57 @@
 let htmlElement = document.querySelector("html");
 let btnToggleMode = document.querySelector(".btnToggleMode");
 
+function toggleBodyTheme(themeName) {
+  htmlElement.setAttribute("data-bs-theme", themeName);
+}
+
+function toggleHeroSectionTheme(backColor) {
+  /* hero section */
+  let hero_sec = document.querySelector("#hero-section");
+  hero_sec.style.backgroundColor = backColor;
+}
+
+function toggleAchievementsStyle(backColor, textColor) {
+  let ach = document.querySelectorAll(
+    "#about .container-fluid .aboutMeContent .c2 .achievements .ach",
+  );
+  ach.forEach((item) => {
+    item.style.color = textColor;
+    item.style.backgroundColor = backColor;
+  });
+}
+
+function toggleSocialMediaIconsStyle(backColor, textColor) {
+  let ach = document.querySelectorAll(
+    "#about .container-fluid .aboutMeContent .c2 .socialMediaIcons .socialMediaIcon",
+  );
+  ach.forEach((item) => {
+    item.style.color = textColor;
+    item.style.backgroundColor = backColor;
+  });
+}
+
 btnToggleMode.addEventListener("click", function (event) {
   /* general change */
   let currentMode = htmlElement.getAttribute("data-bs-theme");
-  /* hero section */
-  let hero_sec = document.querySelector("#hero-section");
-  console.log(currentMode);
+
+  // console.log(currentMode);
 
   if (currentMode.toLowerCase() === "dark") {
-    htmlElement.setAttribute("data-bs-theme", "light");
-    hero_sec.style.backgroundColor = "#fff";
+    toggleBodyTheme("light");
+    toggleHeroSectionTheme("#fff");
+    toggleAchievementsStyle("#f8fafc", "#212529");
+    toggleSocialMediaIconsStyle("#f8fafc", "#212529");
   } else if (currentMode.toLowerCase() === "light") {
-    htmlElement.setAttribute("data-bs-theme", "dark");
-    hero_sec.style.backgroundColor = "#2B3035";
+    toggleBodyTheme("dark");
+    toggleHeroSectionTheme("#44484c");
+    toggleAchievementsStyle("#44484C", "#fff");
+    toggleSocialMediaIconsStyle("#44484C", "#fff");
   } else {
-    htmlElement.setAttribute("data-bs-theme", "light");
-    hero_sec.style.backgroundColor = "#fff";
+    toggleBodyTheme("light");
+    toggleHeroSectionTheme("#fff");
+    toggleAchievementsStyle("#f8fafc", "#212529");
+    toggleSocialMediaIconsStyle("#f8fafc", "#212529");
   }
 
   event.stopPropagation();
